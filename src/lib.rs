e15ad19fad567f673860c95d70656e02a355abe3
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-11-21 20:15:18 +0200

    update lib.rs formatting

--- src/lib.rs
+++ src/lib.rs
@@ -109,16 +109,15 @@ pub fn compile_shader(
     context.shader_source(&shader, source);
     context.compile_shader(&shader);
 
-    if context
+    match context
         .get_shader_parameter(&shader, Ctx::COMPILE_STATUS)
         .as_bool()
         .unwrap_or(false)
     {
-        Ok(shader)
-    } else {
-        Err(context
+        true => Ok(shader),
+        false => Err(context
             .get_shader_info_log(&shader)
-            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+            .unwrap_or_else(|| String::from("Unknown error creating shader"))),
     }
 }
 
@@ -135,16 +134,15 @@ pub fn link_program(
     context.attach_shader(&program, frag_shader);
     context.link_program(&program);
 
-    if context
+    match context 
         .get_program_parameter(&program, Ctx::LINK_STATUS)
         .as_bool()
         .unwrap_or(false)
     {
-        Ok(program)
-    } else {
-        Err(context
+        true => Ok(program),
+        false => Err(context
             .get_program_info_log(&program)
-            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+            .unwrap_or_else(|| String::from("Unknown error creating program object"))),
     }
 }
 
