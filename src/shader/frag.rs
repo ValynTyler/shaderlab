5457400fb31f4955072c28f52891d381e99d2a43
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-11-22 21:15:55 +0200

    impl `Shader` on vertex and fragment shader structs

--- src/shader/frag.rs
+++ src/shader/frag.rs
@@ -1,7 +1,15 @@
-use web_sys::WebGlShader;
+use web_sys::{WebGl2RenderingContext, WebGlShader};
+
+use super::Shader;
 
 pub struct FragmentShader {
     gl_shader: WebGlShader,
+}
+
+impl Shader for FragmentShader {
+    fn gl_enum() -> u32 {
+        WebGl2RenderingContext::FRAGMENT_SHADER
+    }
 }
 
 impl From::<WebGlShader> for FragmentShader {
--- src/shader/vert.rs
+++ src/shader/vert.rs
@@ -1,7 +1,15 @@
-use web_sys::WebGlShader;
+use web_sys::{WebGl2RenderingContext, WebGlShader};
+
+use super::Shader;
 
 pub struct VertexShader {
     gl_shader: WebGlShader,
+}
+
+impl Shader for VertexShader {
+    fn gl_enum() -> u32 {
+        WebGl2RenderingContext::VERTEX_SHADER
+    }
 }
 
 impl From::<WebGlShader> for VertexShader {
