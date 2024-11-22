bbb8a817ee690bb7ebeba714d0fa45e398467091
Valyn Tyler <git@valyntyler.com>
2024-11-22 23:07:46 +0200

    fix: lib.rs docs

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ fn start() -> Result<(), JsValue> {
     let program = Program::link(&context, &vert_shader, &frag_shader)?;
     context.use_program(Some(&program.gl_program));
 
-    // quad vertices
+    // triangle vertices
     let vertices: [f32; 9] = [
         -0.7, -0.7,  0.0,
          0.7, -0.7,  0.0,
